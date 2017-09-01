4854c6e61da209c50202be4ae3dfeace446da0d9
Gogol Bhattacharya <gogolB@users.noreply.github.com>
2017-09-01 00:11:25 -0700

    Updated PRNG.h

--- includes/PRNG.h
+++ includes/PRNG.h
@@ -1,7 +1,8 @@
 /*
  * PRNG.h
- *
- * Created: 8/23/2017 1:47:03 PM
+ * Generates a pseduorandom number using the XORshift algorithm
+ * 
+ *  Created: 8/23/2017 1:47:03 PM
  *  Author: Gogol
  */ 
 
@@ -12,8 +13,14 @@
 // Re-init with a given seed
 void Initialize(uint8_t  seed);
 
+// Returns a random number between 0 and 255
 uint8_t getRandomNumber();
 
+// Returns a random LED pattern from the following
+// 1) 0000 0001
+// 2) 0000 0010
+// 3) 0000 0100
+// 4) 0000 1000
 uint8_t getRandomLed();
 
 #endif /* PRNG_H_ */
